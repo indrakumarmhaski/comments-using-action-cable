4c3ae498b85d691ab1f49f7c441b771ab0485f9d
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-06-15 16:58:00 +0530

    Created a comment broadcast, configured the websocket routesf, and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users
   root to: "blogs#index"
   resources :blogs
+
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
